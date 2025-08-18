06ba1d14352706a27468c7f1ceb1b1cdbe3392f0
Gian Accel Hernandez <accelvivas@gmail.com>
2025-08-18 15:08:57 +0800

    update

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
+if (isset($_POST['btn_1'])) {
+    echo "NU Bulldogs";
+}
 
 
+if (isset($_POST['btn_2'])) {
+    echo "Nationalians";
+}
 ?>
 
 <form method="POST">
 
 
+<input type="submit" name="btn_1" value="Press 1">
+
+<input type="submit" name="btn_2" value="Press 2">
 
 </form>
